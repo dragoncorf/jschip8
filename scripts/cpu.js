36abd6ad2c8e24f33798df9893d2145802a83c20
Diego <diegocorf123@gmail.com>
2024-06-04 20:47:14 -0600

    feat: add first instructions to cpu

--- scripts/cpu.js
+++ scripts/cpu.js
@@ -116,12 +116,138 @@ class CPU {
 		}
 	}
 
-	executeInstruction() {
+	executeInstruction(opcode) {
+		// Each instruction is 2 bytes long, so increment it by 2.
 		this.pc += 2;
 
+		// This gets the lower 4 bits of the high byte by bitwise AND operation,
+		// shifts the 8 bits to the right to leave that value alone
 		let x = (opcode & 0x0f00) >> 8;
 
+		// This gets the higher 4 bits of the low byte by bitwise AND operation,
+		// shifts the 4 bits to the right to leave that value alone
 		let y = (opcode & 0x00f0) >> 4;
+
+		switch (opcode & 0xf000) {
+			case 0x0000:
+				switch (opcode) {
+					case 0x00e0:
+						this.renderer.clear();
+						break;
+					case 0x00ee:
+						this.pc = this.stack.pop();
+						break;
+				}
+
+				break;
+			case 0x1000:
+				this.pc = opcode & 0xfff;
+				break;
+			case 0x2000:
+				this.stack.push(this.pc);
+				this.pc = opcode & 0xfff;
+				break;
+			case 0x3000:
+				if (this.v[x] === (opcode & 0xff)) {
+					this.pc += 2;
+				}
+				break;
+			case 0x4000:
+				if (this.v[x] !== (opcode & 0xff)) {
+					this.pc += 2;
+				}
+				break;
+			case 0x5000:
+				if (this.v[x] === this.v[y]) {
+					this.pc += 2;
+				}
+				break;
+			case 0x6000:
+				this.v[x] = opcode & 0xff;
+				break;
+			case 0x7000:
+				this.v[x] += opcode & 0xff;
+				break;
+			case 0x8000:
+				switch (opcode & 0xf) {
+					case 0x0:
+						this.v[x] = this.v[y];
+						break;
+					case 0x1:
+						this.v[x] |= this.v[y];
+						break;
+					case 0x2:
+						this.v[x] &= this.v[y];
+						break;
+					case 0x3:
+						this.v[x] ^= this.v[y];
+						break;
+					case 0x4:
+						let sum = (this.v[x] += this.v[y]);
+
+						this.v[0xf] = 0x0;
+
+						if (sum > 0xff) {
+							this.v[0xf] = 0x1;
+						}
+						break;
+					case 0x5:
+						break;
+					case 0x6:
+						break;
+					case 0x7:
+						break;
+					case 0xe:
+						break;
+				}
+
+				break;
+			case 0x9000:
+				break;
+			case 0xa000:
+				break;
+			case 0xb000:
+				break;
+			case 0xc000:
+				break;
+			case 0xd000:
+				break;
+			case 0xe000:
+				switch (opcode & 0xff) {
+					case 0x9e:
+						break;
+					case 0xa1:
+						break;
+				}
+
+				break;
+			case 0xf000:
+				switch (opcode & 0xff) {
+					case 0x07:
+						break;
+					case 0x0a:
+						break;
+					case 0x15:
+						break;
+					case 0x18:
+						break;
+					case 0x1e:
+						break;
+					case 0x29:
+						break;
+					case 0x33:
+						break;
+					case 0x55:
+						break;
+					case 0x65:
+						break;
+				}
+
+				break;
+
+			default:
+				throw new Error('Unknown opcode ' + opcode);
+		}
 	}
 }
 
